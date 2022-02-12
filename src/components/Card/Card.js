5db68ae5ad1276851d1a3ef7dee07fe5977aa5a2
LE DINH Alban <79143540+LEDINHAlban@users.noreply.github.com>
2022-02-12 21:49:15 +0100

    feat: send name of the card flipped to the parent component via the react context

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import Button from 'react-bootstrap/Button'
 import PropTypes from 'prop-types'
+import { CardsContext } from '../../context'
 import './Card.css'
 import * as global from '../../utils/global'
 import reactIcon from '../../assets/deck/reactIcon.png'
@@ -8,6 +9,15 @@ import vueIcon from '../../assets/deck/vueIcon.png'
 import nodeIcon from '../../assets/deck/nodeIcon.png'
 
 export default function Card({ name }) {
+  const [clicked, setClicked] = useState(false)
+  const cardsFlipped = useContext(CardsContext)
+  const { dispatchCardsEvent } = useContext(CardsContext)
+
+  const handleClick = () => {
+    setClicked(true)
+    dispatchCardsEvent('CARD_FLIPPED', name)
+  }
+
   let cardName
   switch (name) {
     case global.NODE_ICON:
@@ -22,13 +32,21 @@ export default function Card({ name }) {
     default:
       break
   }
-  return (
-    <>
-      <Button className="card" onClick={() => console.log('toto')}>
-        <img className="card-logo" src={cardName} alt="Logo" />
-      </Button>
-    </>
-  )
+  if (clicked) {
+    return (
+      <>
+        <Button className="card">
+          <img className="card-logo" src={cardName} alt="Logo" />
+        </Button>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <Button className="card" onClick={() => handleClick()}></Button>
+      </>
+    )
+  }
 }
 
 Card.propTypes = {
--- src/components/Play/Play.js
+++ src/components/Play/Play.js
@@ -1,16 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Card from '../Card/Card'
 import { shuffleArray } from '../../utils/shuffleArray'
 import * as global from '../../utils/global'
+import { CardsContext } from '../../context'
 
 export default function Play() {
-  const numberOfCards = []
+  const [cardsFlipped, setCardsFlipped] = useState([])
+  const cardsFlippedTmp = [...cardsFlipped]
+  const [numberOfCards, setNumberOfCards] = useState([])
+  const numberOfCardsTmp = [...numberOfCards]
   const cardNames = [global.NODE_ICON, global.REACT_ICON, global.VUE_ICON]
   const AllCardNames = cardNames.concat(cardNames)
   const cardNamesShuffle = shuffleArray(AllCardNames)
 
-  for (let i = 0; i < cardNamesShuffle.length; i++) {
-    numberOfCards.push(<Card key={i} name={cardNamesShuffle[i]} />)
+  const dispatchCardsEvent = (actionType, payload) => {
+    switch (actionType) {
+      case 'CARD_FLIPPED':
+        cardsFlippedTmp.push(payload)
+        setCardsFlipped(cardsFlippedTmp)
+        return
+      default:
+        return
+    }
   }
-  return numberOfCards.map((el) => el)
+
+  console.log('cardsFlipped', cardsFlipped)
+  useEffect(() => {
+    for (let i = 0; i < cardNamesShuffle.length; i++) {
+      numberOfCardsTmp.push(<Card key={i} name={cardNamesShuffle[i]} />)
+      setNumberOfCards(numberOfCardsTmp)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (cardsFlipped.length === 2) {
+      // if cards names are the same
+      if (cardsFlipped[0] === cardsFlipped[1]) {
+        console.log('the two cards matched')
+      } else {
+        console.log('flip again the two cards')
+      }
+      setCardsFlipped([])
+    }
+  }, [cardsFlipped])
+
+  console.log('cardsFlipped', cardsFlipped)
+  return (
+    <CardsContext.Provider value={{ dispatchCardsEvent }}>
+      {numberOfCards.map((el) => el)}
+    </CardsContext.Provider>
+  )
 }
